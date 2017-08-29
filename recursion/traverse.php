94c73ef8e1301d3483d2dba789c2aa7a16e3a7f0
Kenenth Kristensen <kehk@Kenenths-MacBook-Pro.local>
2017-08-29 13:51:20 +0200

    Add basic traverse ex

---
+++ recursion/traverse.php
@@ -0,0 +1,47 @@
+<?php
+
+class traverse{
+
+	/**
+	 * Basic traversing of an multidimensional array and filter out node
+	 *
+	 *	Ex:
+	 *	options[
+	 *		0 => {
+	 *			nodeId = 666,
+	 *			options = [
+	 *				...
+	 *			]
+	 *		}
+	 *	]
+	 *
+	 * @param  array  &$aOptions . Passed by reference so we dont need to return data
+	 */
+	public function testTraverse(&$aOptions){
+
+		foreach($aOptions as $key => &$oOption){
+
+			//  Base case, stop recusrion on this branch
+			//  Remove from options array
+			if($oOption->nodeId == 14){
+				error_log("FOUND... remove it");
+				unset($aOptions[$key]);
+				return;
+			}
+
+			//  Next...
+			if(!empty($oOption->options)){
+				$this->testTraverse($oOption->options);
+			}
+		}
+	}
+
+
+
+
+
+
+}
+
+
+?>
